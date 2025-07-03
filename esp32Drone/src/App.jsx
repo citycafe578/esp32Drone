f1ec73107e046441c34af8a34ded2339b9302ae7
city cafe <131968937+citycafe578@users.noreply.github.com>
2025-07-03 22:26:55 +0800

    新增其他資料顯示

--- esp32Drone/src/App.jsx
+++ esp32Drone/src/App.jsx
@@ -1,34 +1,40 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-const Card = ({title}) => {
-  const [hasLiked, setHasLiked] = useState(false)
-
-  useEffect(() => {
-    console.log(`${title} has been change`)
-  });
-
-  return (
-    <div className='card'>
-      <h2>{title}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? 'A': 'B'}
-      </button>
+const OtherDataItem = ({title, return_data}) => {
+  return(
+    <div className='other_item' style={{display: 'flex', flexDirection: 'column', backgroundColor: 'gray', width: '340px', height: '180px', margin: '15px', borderRadius: '10px'}}>
+      <div style={{display: 'flex', width: '100%', height: '150px', alignItems: 'center', justifyContent: 'center'}}>
+        <h2>{title}</h2>
+      </div>
+      <div style={{display: 'flex', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center'}}>
+        <h1>return_data</h1>
+      </div>
     </div>
-  );
+  )
 };
 
 const App = () => {
   return (
-    <div id='digital_display'>
-      <div id='stream' className='digital_item'>
-        
+    <>
+      <div id='digital_display'>
+        <div id='stream' className='digital_item'>
+          <h1>stream</h1>
+        </div>
+        <div id='map' className='digital_item'>
+          <h1>map</h1>
+        </div>
       </div>
-      <div id='map' className='digital_item'>
-        
+
+      <div id='other_display' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <OtherDataItem title = 'COURSE' return_data = ' ' />
+        <OtherDataItem title = 'HEIGHT' return_data = ' ' />
+        <OtherDataItem title = '123' return_data = ' ' />
+        <OtherDataItem title = '456' return_data = ' ' />
+        <OtherDataItem title = '789' return_data = ' ' />
       </div>
-    </div>
+    </>
   );
-};
+}
 
 export default App;
